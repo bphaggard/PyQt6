a5b26b7d687f0d69f92b4c68a2b30b72d6d22c93
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2024-12-03 16:37:21 +0100

    ComboBox S2E30-31

---
+++ Part_2/comboBox.py
@@ -0,0 +1,47 @@
+from PyQt6.QtCore import QTime, QTimer
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLCDNumber, QHBoxLayout, QLabel, QComboBox
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(200, 200, 500, 200)
+        self.setWindowTitle("PyQt6 QComboBox")
+
+        self.create_combo()
+
+    def create_combo(self):
+        hbox = QHBoxLayout()
+
+        label = QLabel("Select Account Type: ")
+        label.setFont(QFont("Times", 15))
+
+        self.combo = QComboBox()
+        self.combo.addItem("Current Account")
+        self.combo.addItem("Deposite Account")
+        self.combo.addItem("Saving Account")
+        self.combo.currentTextChanged.connect(self.combo_changed)
+
+        vbox = QVBoxLayout()
+        self.label_result = QLabel("")
+        self.label_result.setFont(QFont("Times", 15))
+        vbox.addWidget(self.label_result)
+        vbox.addLayout(hbox)
+
+        hbox.addWidget(label)
+        hbox.addWidget(self.combo)
+
+        self.setLayout(vbox)
+
+    def combo_changed(self):
+        item = self.combo.currentText()
+        self.label_result.setText("Your Account Type Is : " + item)
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
